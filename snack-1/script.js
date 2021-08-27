d887b60cd66ee9ff129fae1cd4c92d445a5705cd
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-27 12:09:49 +0200

    Aggiunta while al ciclo for

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,22 +1,25 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
-// var somma = 0;
+var somma = 0;
 
-// for ( var i = 0; i < 5; i++ ) {
-//     var numero = parseInt(prompt("Scegli un numero"));
-//     var somma = somma + numero;
-// }
-// console.log(somma);
+for ( var i = 0; i < 5; i++ ) {
+    var numero = parseInt(prompt("Scegli un numero"));
+    while (isNaN(numero)) {
+        numero =parseInt(prompt("Attento! Inserisci un numero"));
+    }
+    var somma = somma + numero;
+}
+console.log(somma);
 
 
 // Esegui questo programma in due versioni, con il for e con il while.
-var somma = 0;
-var i = 1;
+// var somma = 0;
+// var i = 1;
 
-while (i <= 5){
-    var numero = parseInt(prompt("Scegli un numero"));
-    var somma = somma + numero;
-    i++
-}
-console.log(somma);
+// while (i <= 5){
+//     var numero = parseInt(prompt("Scegli un numero"));
+//     var somma = somma + numero;
+//     i++
+// }
+// console.log(somma);
 
