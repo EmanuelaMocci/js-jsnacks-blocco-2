2d9500d1464122d70881749552e674e7667e3dad
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-08-27 15:21:36 +0200

    Aggiunta esercizio snack 4

---
+++ snack-4/script.js
@@ -0,0 +1,20 @@
+// Crea un array di numeri interi
+var numeri = [10, 15, 17, 24, 56, 78];
+var numeriDispari = [];
+var sum = 0;
+
+// Fai la somma di tutti gli elementi che sono in posizione (indice dell'array) dispari
+
+for (var i = 0; i < numeri.length; i++) {
+    if(i % 2 === 1) { 
+        numeriDispari.push(numeri[i]);
+        result = sum += numeri[i];
+    }
+}
+console.log(numeriDispari);
+console.log(result);
+
+
+
+
+
